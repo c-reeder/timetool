5c4f810c5a3e2b01c7f3722831b7369474edc5e0
Connor Reeder <connor.reeder@ymail.com>
2022-08-26 11:29:38 -0400

    fixed formatting for db

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 	"time"
@@ -10,23 +11,61 @@ func printError(msg string) {
 	fmt.Fprintln(os.Stderr, msg)
 }
 
+// Default format for timestamp with tz in DBeaver
+const dbFormat = "2006-01-02 15:04:05.000 -0700"
+
 func main() {
-	if len(os.Args) != 3 {
-		printError("Must receive 2 arguments!")
+	var formatStr = flag.String("f", "3339", "The format of timestamp to use")
+	flag.Parse()
+	args := flag.Args()
+
+	if len(args) < 1 {
+		printError("Must provide command")
 		os.Exit(-1)
 	}
 
-	t1, err := time.Parse(time.RFC3339, os.Args[1])
+	var format string
+	switch *formatStr {
+	case "3339":
+		format = time.RFC3339
+	case "db":
+		format = dbFormat
+	default:
+		format = time.RFC3339
+	}
+
+	switch args[0] {
+	case "now":
+		now(format)
+	case "diff":
+		diff(format, args[1:])
+	default:
+		now(format)
+	}
+}
+
+func now(format string) {
+	t1 := time.Now()
+	fmt.Println(t1.Format(format))
+}
+
+func diff(format string, args []string) {
+	if len(args) != 2 {
+		printError("Must provide two arguments to diff")
+		os.Exit(-1)
+	}
+
+	t1, err := time.Parse(format, args[0])
 	if err != nil {
-		fmt.Println(os.Args[1])
+		fmt.Println(args[1])
 		printError("Error parsing first time stamp")
 		printError(err.Error())
 		os.Exit(-1)
 	}
 
-	t2, err := time.Parse(time.RFC3339, os.Args[2])
+	t2, err := time.Parse(format, args[1])
 	if err != nil {
-		fmt.Println(os.Args[2])
+		fmt.Println(args[2])
 		printError("Error parsing second time stamp")
 		os.Exit(-1)
 	}
